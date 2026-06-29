bd269fe72161722f49b9a4dc4cdf868610a9afe5
sooncheul <tnscjf3146@naver.com>
2026-06-29 20:54:13 +0900

    setting

---
+++ project/front/nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+  ],
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+        '@vueuse/nuxt',
+      ]
+    }
+  }
+})
